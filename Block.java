8505481a9d775156a8445ed1b223fd67fe65e109
VakoShark <112679774+VakoShark@users.noreply.github.com>
2023-12-05 14:50:42 -0600

    Update Block.java

--- Block.java
+++ Block.java
@@ -15,9 +15,13 @@ public class Block {
     public int getY() {
         return y;
     }
-
-    public void move(int dx, int dy) {
-        x += dx;
-        y += dy;
+    
+    public void setX(int x) {
+    	this.x = x;
     }
+    
+    public void setY(int y) {
+    	this.y = y;
+    }
+
 }
