a04e9bead51acc176d2eb1ac7d873605f98f87d9
nneal5261 <119890722+nneal5261@users.noreply.github.com>
2023-12-06 16:20:18 -0600

    Update BlockPushingGame.java
    This is another update with wall collision and a victory condition

--- BlockPushingGame.java
+++ BlockPushingGame.java
@@ -13,12 +13,12 @@ public class BlockPushingGame extends JPanel
 private final int WIDTH = 700, HEIGHT = 500;
 private final int JUMP = 10;  // increment for image movement
 
-private final int IMAGE_SIZE = 1;
-
 private ImageIcon up, down, right, left, currentImage, pushBlock, goalBlock;
 private int x, y;
-private int pushBlockX = 300;
-private int pushBlockY = 250;
+public int pushBlockX = 300;
+public int pushBlockY = 250;
+private int goalBlockX = 650;
+private int goalBlockY = 450;
 
 //-----------------------------------------------------------------
 //  Constructor: Sets up this panel and loads the images.
@@ -66,30 +66,42 @@ private class DirectionListener implements KeyListener
   //  Responds to the user pressing arrow keys by adjusting the
   //  image and image location accordingly.
   //--------------------------------------------------------------
+	
+	private int nextX, nextY;
+	
   public void keyPressed (KeyEvent event)
   {  
-	  int nextX = x;
-	  int nextY = y;
+	  nextX = x;
+	  nextY = y;
 	  
-     switch (event.getKeyCode())
-     {
-        case KeyEvent.VK_UP:
-           nextY -= JUMP;
-           break;
-        case KeyEvent.VK_DOWN:
-           nextY += JUMP;
-           break;
-        case KeyEvent.VK_LEFT:
-           nextX -= JUMP;
-           break;
-        case KeyEvent.VK_RIGHT:
-           nextX += JUMP;
-           break;
-     }
+	 if (!victoryCondition(nextX, nextY)) {
+	     switch (event.getKeyCode())
+	     {
+	        case KeyEvent.VK_UP:
+	           nextY -= JUMP;
+	           break;
+	        case KeyEvent.VK_DOWN:
+	           nextY += JUMP;
+	           break;
+	        case KeyEvent.VK_LEFT:
+	           nextX -= JUMP;
+	           break;
+	        case KeyEvent.VK_RIGHT:
+	           nextX += JUMP;
+	           break;
+	     }
+	 }
+	 else {
+		 System.out.println("Congratulations, You Won!");
+	 }
      
      if (!checkCollision(nextX, nextY)) {
     	 x = nextX;
     	 y = nextY;
+     }
+     if (wallCollision(nextX, nextY)) {
+    	 x -= nextX;
+    	 y -= nextY;
      }
      
      repaint();
@@ -101,6 +113,7 @@ private class DirectionListener implements KeyListener
   public void keyTyped (KeyEvent event) {}
   public void keyReleased (KeyEvent event) {}
 }
+
 
 private boolean checkCollision(int newX, int newY) {
     // Check for collision with push block
@@ -119,5 +132,21 @@ private boolean checkCollision(int newX, int newY) {
     	System.out.println("You Reached the Goal!");
     }
     return false; // No collision
+}
+
+private boolean wallCollision(int newX, int newY) {
+    // Check for collision with push block
+    if (newX >= 670 || newX < 0 || newY >= 500 || newY < 0) {
+        // Check if the next position after pushing is within bounds
+        return true; // Collision successful, push_block moved
+        }
+    return false; // No collision
+}
+
+private boolean victoryCondition(int newX, int newY) {
+	if(pushBlockX == goalBlockX && pushBlockY == goalBlockY) {
+		return true;
+	}
+	return false;
 }
 }
