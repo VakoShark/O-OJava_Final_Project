54e235c1323989ddbef11401be6a219f950da0f8
nneal5261 <119890722+nneal5261@users.noreply.github.com>
2023-12-07 17:42:36 -0600

    Update main.java
    Updated main to get rid of file testing comments and to add in the potential for ArrayList usage in level creation

--- main.java
+++ main.java
@@ -1,38 +1,24 @@
 package blockGame;
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
+//Class that contains the driver method for the entirety of the block game
 public class main {
+	
+	//Make this into something that multiple classes can use
+	//Each level will be it's own class
+	private ArrayList<BlockPushingGame> levels = new ArrayList<BlockPushingGame>();
+
 
 	public static void main(String[] args) {
-		
-	    BlockPushingGame bpg = new BlockPushingGame();
-		
-	    JFrame frame = new JFrame ("Block Game");
-	    frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	      JFrame frame = new JFrame ("Block Game");
+	      frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 
-	    frame.getContentPane().add (bpg);
+	      frame.getContentPane().add (new BlockPushingGame());
 
-	    frame.pack();
-	    frame.setLocationRelativeTo(null);
-	    frame.setVisible(true);
-	}
-/*		        String imageName = "block.png";
-
-		        if (imageExists(imageName)) {
-		            System.out.println("Image exists!");
-		        } else {
-		            System.out.println("Image does not exist.");
-		        }
-		    }
-
-		    public static boolean imageExists(String imageName) {
-		        // Get the current working directory
-		        String currentDirectory = System.getProperty("user.dir");
-
-		        // Create a Path object for the image file in the current directory
-		        Path imagePath = FileSystems.getDefault().getPath(currentDirectory, imageName);
-
-		        // Check if the file exists
-		        return Files.exists(imagePath) && !Files.isDirectory(imagePath);
-		    }*/
+	      frame.pack();
+	      frame.setLocationRelativeTo(null);
+	      frame.setVisible(true);	
+	      }
 }
