5c88794cca8bef1d7075e4f0a2c12e1421084f24
VakoShark <112679774+VakoShark@users.noreply.github.com>
2023-12-14 12:47:33 -0600

    Final update

--- main.java
+++ main.java
@@ -1,24 +1,17 @@
 package blockGame;
 
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
-//Class that contains the driver method for the entirety of the block game
 public class main {
-	
-	//Make this into something that multiple classes can use
-	//Each level will be it's own class
-	private ArrayList<BlockPushingGame> levels = new ArrayList<BlockPushingGame>();
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Push Block Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	public static void main(String[] args) {
-	      JFrame frame = new JFrame ("Block Game");
-	      frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-
-	      frame.getContentPane().add (new BlockPushingGame());
-
-	      frame.pack();
-	      frame.setLocationRelativeTo(null);
-	      frame.setVisible(true);	
-	      }
+        BlockPushingGame pushBlockGame = new BlockPushingGame();
+        frame.getContentPane().add(pushBlockGame);
+        frame.setSize(513, 535);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+    };
 }
